5d55f1ab73deb4e735f1be40f534adf0cc89d1ad
Roy Ronalds <tchalvakspam@gmail.com>
2014-12-17 09:31:51 -0500

    INIT: Passed the initial test cases into romeo.txt and rain.txt, and created the very simple test.php file within which I'll test the function & expected results.

---
+++ tests.php
@@ -0,0 +1,9 @@
+<?php
+
+//  List the files to check through, and run through each file, creating the output from each.
+$files = array(
+	'./romeo.txt',
+	'./rain.text',
+	'./jabberwock.txt',
+	'./alice.txt');
+// I added an additional set of test cases just to make sure the process was working beyond the two examples.
