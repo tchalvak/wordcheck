7b5c3bef58ed1917f3a8ea7e67c62fc25dd5ce56
Roy Ronalds <tchalvakspam@gmail.com>
2014-12-17 09:38:04 -0500

    Test: Create some expected outputs and the initial function.

--- tests.php
+++ tests.php
@@ -7,3 +7,18 @@ $files = array(
 	'./jabberwock.txt',
 	'./alice.txt');
 // I added an additional set of test cases just to make sure the process was working beyond the two examples.
+
+$expected_results = array(
+	'wherefore',
+	'people',
+	'',
+	'',
+
+// Simple function to take files in and return their most lettered word
+function find_most_lettered_word($path){
+	$contents = file_get_contents($path);
+	/*
+	The text sample will contain only the alphabetic characters ("a" through "z" and "A" through "Z"), spaces, and punctuation marks. A letter is considered to be the same letter regardless of whether it appears in uppercase or lowercase. The words will be separated by spaces, and any punctuation marks should be disregarded.
+	*/
+
+}
