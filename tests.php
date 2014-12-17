95bc3f996aa99f44a7c7678e50da7a7dd93d741b
Roy Ronalds <tchalvakspam@gmail.com>
2014-12-17 09:49:36 -0500

    Test: Put in my expected results for jabberwock & alice quotes, and comment out the full solution before actually laying down the code.

--- tests.php
+++ tests.php
@@ -11,14 +11,38 @@ $files = array(
 $expected_results = array(
 	'wherefore',
 	'people',
-	'',
-	'',
+	'jabberwock',
+	'people',
 
 // Simple function to take files in and return their most lettered word
 function find_most_lettered_word($path){
 	$contents = file_get_contents($path);
 	/*
-	The text sample will contain only the alphabetic characters ("a" through "z" and "A" through "Z"), spaces, and punctuation marks. A letter is considered to be the same letter regardless of whether it appears in uppercase or lowercase. The words will be separated by spaces, and any punctuation marks should be disregarded.
+	The text sample will contain only the alphabetic characters ("a" through "z" and "A" through "Z"), 
+	spaces, and punctuation marks. 
+	A letter is considered to be the same letter regardless of whether it appears in uppercase or lowercase. 
+	The words will be separated by spaces, and any punctuation marks should be disregarded.
 	*/
 
+	// Normalize the text first:
+	// Make everything lowercase
+	// Strip multiple whitespaces
+	// Filter to alphanumeric and spaces
+
+	// Tokenize the string into words.
+
+	// Store the winning word (none to start)
+	// Store the max character count (0 to start)
+	// Get a character count per wordletter, store the max character count.
+	// If the max is more than the stored max, change the winning word.
+
+	// return the winning word
+
+
 }
+
+
+
+// Loop over the text cases and get their outputs
+
+// Check each output against each expected result.
